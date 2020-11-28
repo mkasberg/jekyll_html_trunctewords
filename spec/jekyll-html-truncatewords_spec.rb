7fdab82cddf6441b0a56665e70775fdb39fec5ce
Mike Kasberg <kasberg.mike@gmail.com>
2020-11-28 15:42:23 -0700

    Clean up tests

--- spec/jekyll-html-truncatewords_spec.rb
+++ spec/jekyll-html-truncatewords_spec.rb
@@ -5,50 +5,54 @@ RSpec.describe Jekyll::HtmlTruncatewords do
     expect(Jekyll::HtmlTruncatewords::VERSION).not_to be nil
   end
 
+  it "works as a Liquid template" do
+    input = "<p>This is a sentence with more than 5 words.</p>"
+    liquid = "{{ '#{input}' | html_truncatewords: 5, '~!' }}"
+    output = Liquid::Template.parse(liquid).render()
+    expect(output).to eq('<p>This is a sentence with~!</p>')
+  end
+
   context 'examples' do
+    def check(input, words, expected)
+      output = Jekyll::HtmlTruncatewords.html_truncatewords(input, words)
+      expect(output).to eq(expected)
+    end
+
     it "doesn't change input under limit with no HTML" do
-      input = "foo bar"
-      liquid = "{{ '#{input}' | html_truncatewords: 10 }}"
-      output = Liquid::Template.parse(liquid).render()
-      expect(output).to eq(input)
+      input = 'foo bar'
+      check(input, 10, input)
     end
 
     it "truncates words to the limit" do
-      input = 'foo bar'
-      output = Jekyll::HtmlTruncatewords.html_truncatewords(input, 1)
-      expect(output).to eq('foo...')
+      check('foo bar', 1, 'foo...')
     end
 
     it "doesn't count tags as words" do
-      input = "<p>foo</p>"
-      Jekyll::HtmlTruncatewords.html_truncatewords(input)
-      liquid = "{{ '#{input}' | html_truncatewords: 1 }}"
-      output = Liquid::Template.parse(liquid).render()
-      expect(output).to eq(input)
+      input = "<p>foo bar</p>"
+      check(input, 2, input)
     end
 
     it "truncates text inside a single tag" do
-      input = "<p>This is a sentence with more than 5 words.</p>"
-      Jekyll::HtmlTruncatewords.html_truncatewords(input, 5)
-      liquid = "{{ '#{input}' | html_truncatewords: 5 }}"
-      output = Liquid::Template.parse(liquid).render()
-      expect(output).to eq('<p>This is a sentence with...</p>')
+      input = '<p>This is a sentence with more than 5 words.</p>'
+      expected = '<p>This is a sentence with...</p>'
+      check(input, 5, expected)
     end
 
     it "handles sibling tags" do
       input = "<p>foo</p><p>bar</p>"
-      Jekyll::HtmlTruncatewords.html_truncatewords(input, 10)
-      liquid = "{{ '#{input}' | html_truncatewords: 10 }}"
-      output = Liquid::Template.parse(liquid).render()
-      expect(output).to eq(input)
+      check(input, 10, input)
+    end
+
+    it 'truncates sibling tags correctly' do
+      input = '<p>This is the first paragraph.</p><p>This is the second paragraph.</p><p>This is the third paragraph.</p>'
+      expected = '<p>This is the first paragraph.</p><p>This is the...</p>'
+      check(input, 8, expected)
     end
 
     it "handles child tags" do
-      input = "<div>foo <span>bar</span></div>"
-      Jekyll::HtmlTruncatewords.html_truncatewords(input, 1)
-      liquid = "{{ '#{input}' | html_truncatewords: 1 }}"
-      output = Liquid::Template.parse(liquid).render()
-      expect(output).to eq("<div>foo...</div>")
+      input = '<div>foo <span>bar</span></div>'
+      expected = '<div>foo...</div>'
+      check(input, 1, expected)
     end
   end
 end
